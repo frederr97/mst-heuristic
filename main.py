80b7c20043317b491ef4810fc7a8dc1c506d817f
Fred Resende <fredribeiro97@gmail.com>
2018-11-19 14:38:12 -0200

    Prim working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from igraph import *
 
 def initialize():
 	grafo = Graph()
-	arq = open('entrada/v100_d30_t1_i1.dat')
+	arq = open('teste.dat')
 	linha = arq.readline()
 	vertices, arestas = linha.split()		# Receive vertices and arestas
 	grafo.add_vertices(int(vertices))
@@ -33,29 +33,28 @@ def initialize():
 
 def prim(grafo):
 	# Setting the "added" flag of all vertices and edges to False
-	for vertice in grafo.vs:
-		grafo.vs[vertice]["added"] = False
 	arvore = Graph()
 	arvore.add_vertex()
-	print arvore
 	v_add = [0]
 	arestas_arvore = []
+	custo = 0
 	# Loop to construct the minimum spanning tree			 
-	while arvore.gvcount() != grafo.gvcount():
+	while arvore.vcount() != grafo.vcount():
 		aresta_peso = []
 		for vertice in v_add:
 			for aresta in grafo.incident(vertice):
 				aresta_peso.append([aresta, grafo.es[aresta]["weight"]])
 		aresta_peso.sort(key=lambda x: x[1])
-		arestas_arvore.append([aresta_peso[0][0].source, aresta_peso[0][0].target])
+		arestas_arvore.append([grafo.es[aresta_peso[0][0]].source, grafo.es[aresta_peso[0][0]].target])
 		v_add.append(grafo.es[aresta_peso[0][0]].target)
 		grafo.delete_edges(aresta_peso[0][0])
 		custo += aresta_peso[0][1]
 		arvore.add_vertex()
 	# Create the MST with edges list obtained
-	for i in len(arestas_arvore):
+	for i in range(len(arestas_arvore)):
 		arvore.add_edges([(arestas_arvore[i][0], arestas_arvore[i][1])])
 		print arestas_arvore[i]
+	print "O custo da AGM é ", custo	
 		
 
 def main():
