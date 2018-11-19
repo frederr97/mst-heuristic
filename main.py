1da464b3ca7b6e93bf14770d0a1c872e7120529e
Fred Resende <fredribeiro97@gmail.com>
2018-11-19 01:05:52 -0200

    First Prim

--- main.py
+++ main.py
@@ -28,10 +28,39 @@ def initialize():
 		grafo.add_edges([(int(v1),int(v2))])
 		grafo.es[grafo.get_eid(int(v1),int(v2))]["weight"] = int(peso)  
 
-	arq.close()	
+	arq.close()
+	return grafo
+
+def prim(grafo):
+	# Setting the "added" flag of all vertices and edges to False
+	for vertice in grafo.vs:
+		grafo.vs[vertice]["added"] = False
+	arvore = Graph()
+	arvore.add_vertex()
+	print arvore
+	v_add = [0]
+	arestas_arvore = []
+	# Loop to construct the minimum spanning tree			 
+	while arvore.gvcount() != grafo.gvcount():
+		aresta_peso = []
+		for vertice in v_add:
+			for aresta in grafo.incident(vertice):
+				aresta_peso.append([aresta, grafo.es[aresta]["weight"]])
+		aresta_peso.sort(key=lambda x: x[1])
+		arestas_arvore.append([aresta_peso[0][0].source, aresta_peso[0][0].target])
+		v_add.append(grafo.es[aresta_peso[0][0]].target)
+		grafo.delete_edges(aresta_peso[0][0])
+		custo += aresta_peso[0][1]
+		arvore.add_vertex()
+	# Create the MST with edges list obtained
+	for i in len(arestas_arvore):
+		arvore.add_edges([(arestas_arvore[i][0], arestas_arvore[i][1])])
+		print arestas_arvore[i]
+		
 
 def main():
-	initialize()
+	grafo = initialize()
+	prim(grafo)
 
 if __name__ == '__main__':
     main()
